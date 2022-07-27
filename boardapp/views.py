351654e2f166193a566afd83baa5fda1478da04e
KentaroYoshizumi <schecter3.2.1-@softbank.ne.jp>
2022-07-27 22:52:50 +0900

    bootstrap

--- boardapp/views.py
+++ boardapp/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
 
 # Create your views here.
+
+
+def signupfunc(request):
+    object = User.objects.get(username='tanaka')
+    print(object.email)
+    if request.method == "POST":
+        print("This is post method.")
+    else:
+        print("This is not post method.")
+    return render(request, 'signup.html', {'some':100})
